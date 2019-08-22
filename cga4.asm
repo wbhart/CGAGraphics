2cf8801b5d9879543cf98b537be43d5799378a81
William Hart <goodwillhart@googlemail.com>
2019-08-22 22:49:25 +0200

    Reorder some registers and remove an instruction. Patch colours.

--- cga4.asm
+++ cga4.asm
@@ -6,8 +6,8 @@
 _cga_draw_line1 PROC
    ARG x0:WORD, y0:WORD, xdiff:WORD, ydiff:WORD, D:WORD, xend:WORD, colour:BYTE
    ; line from (x0, y0) - (xend, ?) including endpoints
-   ; AX: Acc, BX: 2*dx, DX: 2*dy, CX: Loop, BP: ydelta
-   ; SI: D, DI: Offset, DS: Colour/Mask, ES: B800
+   ; AX: Acc, BX: 2*dx, SP: 2*dy, CX: Loop, BP: ydelta
+   ; DX: D, DI: Offset, ES: B800, SI: Jump offset
    push bp
    mov bp, sp
    push di
@@ -39,14 +39,14 @@ line1_y_even:
    mov bx, [xdiff]      ; compute 2*dx
    shl bx, 1
 
-   mov dx, [ydiff]      ; compute 2*dy
-   shl dx, 1
+   mov si, [ydiff]      ; compute 2*dy
+   shl si, 1
 
 line1_yinc:
 
-   mov si, [D]          ; store D
+   mov dx, [D]          ; store D
 
-   mov cx, [x0]         ; set up colour and mask
+   mov cx, [x0]         ; compute jump offset
    and cl, 3            ; multiply x mod 4 by 20
    shl cl, 1
    shl cl, 1
@@ -56,9 +56,29 @@ line1_yinc:
    add al, cl
    xor ah, ah
    mov ds, ax
-   mov ah, [colour] 
+
+   mov ah, [colour]     ; patch colours in
+   ror ah, 1
+   ror ah, 1
+   mov BYTE PTR cs:[line1_patch1 + 1], ah
+   ror ah, 1
+   ror ah, 1
+   mov BYTE PTR cs:[line1_patch2 + 1], ah
+   ror ah, 1
+   ror ah, 1
+   mov BYTE PTR cs:[line1_patch3 + 1], ah
+   ror ah, 1
+   ror ah, 1
+   mov BYTE PTR cs:[line1_patch4 + 1], ah
 
    mov ax, [x0]         ; get x0
+
+   shr ax, 1            ; adjust offset for column x0
+   shr ax, 1
+   add di, ax
+
+   shl ax, 1            ; round x0 down to nearest multiple of 4
+   shl ax, 1
    
    mov cx, [xend]       ; compute loop iterations
    pop bp               ; (get ydelta here so we can push below)
@@ -68,25 +88,21 @@ line1_yinc:
    shr cx, 1
    shr cx, 1            ; we will unroll by 4 so divide by 4
 
-   shr ax, 1            ; adjust offset for column x0
-   shr ax, 1
-   add di, ax
+   sub dx, si           ; compensate for first addition of 2*dy
 
-   sub si, dx           ; compensate for first addition of 2*dy
-
-   cli
+   cli                  ; save and free up sp
    mov WORD PTR cs:[line1_patch1 + 1], sp
-   mov sp, dx
+   mov sp, si
 
-   mov ax, ds           ; get jump offset
-   
-   mov dx, si
+   mov ax, ds           ; get jump offset   
    mov si, ax
    lea si, si + line1_loop
    jmp si
+
 line1_loop:
    mov al, 03fh         
    and al, es:[di]      ; draw pixel at x, y
+line1_patch1:
    or al, 040h
    add dx, sp           ; D += 2*dy
    stosb
@@ -103,6 +119,7 @@ line1_skip_incy1:
 
    mov al, 0cfh
    and al, es:[di]      ; draw pixel at x, y
+line1_patch2:
    or al, 010h
    add dx, sp           ; D += 2*dy
    stosb
@@ -119,6 +136,7 @@ line1_skip_incy2:
 
    mov al, 0f3h
    and al, es:[di]      ; draw pixel at x, y
+line1_patch3:
    or al, 04h
    add dx, sp           ; D += 2*dy
    stosb
@@ -135,6 +153,7 @@ line1_skip_incy3:
 
    mov al, 0fch
    and al, es:[di]      ; draw pixel at x, y
+line1_patch4:
    or al, 01h
    add dx, sp           ; D += 2*dy
    stosb
@@ -153,8 +172,9 @@ line1_patch1:
    mov sp, 1234
    sti
 
-   pop cx
-
+   pop cx               ; do remaining iterations (0-3)
+                        ; TODO
+   
    pop ds
    pop si
    pop di
