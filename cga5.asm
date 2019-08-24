d129a5ef8409fa8085c3af894fbfaed2287b2e8f
William Hart <goodwillhart@googlemail.com>
2019-08-24 11:50:54 +0200

    Add line blanking routine.

--- cga5.asm
+++ cga5.asm
@@ -469,6 +469,7 @@ linex1_skip_incy4:
 
    loop linex1_loop
 
+
 linex1_no_iter:
 
 linex1_patch6:
@@ -808,6 +809,203 @@ linew1_patch5:
    pop bp
    ret
 _cga_draw_line_write1 ENDP
+
+   PUBLIC _cga_draw_line_zero1
+_cga_draw_line_zero1 PROC
+   ARG x0:WORD, y0:WORD, xdiff:WORD, ydiff:WORD, D:WORD, xend:WORD, colour:BYTE
+   ; line from (x0, y0) - (xend, ?) including endpoints, xor'd
+   ; AX: Acc, BX: 2*dx, SP: 2*dy, CX: Loop, BP: ydelta
+   ; DX: D, DI: Offset, ES: B800, SI: Jump offset
+   push bp
+   mov bp, sp
+   push di
+   push si
+   push ds
+
+   mov ax, 0b800h       ; set segment for CGA memory
+   mov es, ax
+
+   xor di, di           ; compute offset for line y0
+   mov ax, [y0]
+
+   shr ax, 1
+   mov bx, 8192         ; also compute ydelta
+   jnc linez1_y_even
+   mov bx, -8112
+linez1_y_even:
+   mov cx, 0c050h
+   sbb di, 0
+
+   mov si, [ydiff]      ; fixups for +ve/-ve slope
+   cmp si, 0
+   jge linez1_pos
+
+   neg si
+   sub bx, 80
+   mov cx, 0ffb0h
+
+linez1_pos:
+
+   push bx
+
+   mov WORD PTR cs:[linez1_patch10 + 2], cx
+   mov WORD PTR cs:[linez1_patch11 + 2], cx
+   mov WORD PTR cs:[linez1_patch12 + 2], cx
+   mov WORD PTR cs:[linez1_patch13 + 2], cx
+            
+   and di, 8192         ; continue computing offset for line y0
+   mov cl, 4
+   xor ah, ah        
+   shl ax, cl
+   add di, ax
+   shl ax, 1
+   shl ax, 1
+   add di, ax
+
+   mov bx, [xdiff]      ; compute 2*dx
+   shl bx, 1
+         
+   shl si, 1            ; compute 2*dy
+
+linez1_yinc:
+
+   mov dx, [D]          ; store D
+
+   mov cx, [x0]         ; compute jump offset
+   and cl, 3            ; multiply x mod 4 by 12
+   shl cl, 1
+   shl cl, 1
+   mov al, cl
+   shl cl, 1
+   add al, cl
+   xor ah, ah
+   mov ds, ax
+
+   mov al, [colour]     ; patch colours in
+   mov ah, al
+   ror al, 1
+   ror al, 1
+   add ah, al
+   ror al, 1
+   ror al, 1
+   add ah, al
+   ror al, 1
+   ror al, 1
+   add ah, al
+   
+   mov BYTE PTR cs:[linez1_patch4 + 3], ah
+   mov BYTE PTR cs:[linez1_patch7 + 3], ah
+
+   mov ax, [x0]         ; get x0
+
+   shr ax, 1            ; adjust offset for column x0
+   shr ax, 1
+   add di, ax
+
+   shl ax, 1            ; round x0 down to nearest multiple of 4
+   shl ax, 1
+   
+   mov cx, [xend]       ; compute loop iterations
+   sub cx, ax
+   inc cx
+   mov BYTE PTR cs:[linez1_patch6 + 1], cl ; save iterations for prologue
+   shr cx, 1
+   shr cx, 1            ; we will unroll by 4 so divide by 4
+
+   pop bp               ; get ydelta
+   
+   sub dx, si           ; compensate for first addition of 2*dy
+
+   cli                  ; save and free up sp
+   mov WORD PTR cs:[linez1_patch5 + 1], sp
+   mov sp, si
+
+   mov ax, ds           ; get jump offset   
+   mov si, ax
+
+   cmp cl, 0            ; check for iterations = 0
+   je linez1_no_iter
+
+   lea si, si + linez1_loop
+   jmp si
+
+linez1_loop:
+   add dx, sp           ; D += 2*dy
+
+   jle linez1_skip_incy1
+ 
+   add di, bp           ; odd <-> even line (reenigne's trick)
+linez1_patch10:
+   xor bp, 1234         ; adjust ydelta
+
+   sub dx, bx           ; D -= 2*dx
+linez1_skip_incy1:
+
+linez1_patch2:
+   add dx, sp           ; D += 2*dy
+
+   jle linez1_skip_incy2
+
+   add di, bp           ; odd <-> even line (reenigne's trick)
+linez1_patch11:
+   xor bp, 1234         ; adjust ydelta
+
+   sub dx, bx           ; D -= 2*dx
+linez1_skip_incy2:             
+
+linez1_patch3:
+   add dx, sp           ; D += 2*dy
+
+   jle linez1_skip_incy3
+
+   add di, bp           ; odd <-> even line (reenigne's trick)
+linez1_patch12:
+   xor bp, 1234         ; adjust ydelta
+
+   sub dx, bx           ; D -= 2*dx
+linez1_skip_incy3:             
+
+   add dx, sp           ; D += 2*dy
+linez1_patch4:
+   mov es:[di], 123
+   
+   jle linez1_skip_incy4
+
+   add di, bp           ; odd <-> even line (reenigne's trick)
+linez1_patch13:
+   xor bp, 1234         ; adjust ydelta
+
+   sub dx, bx           ; D -= 2*dx
+linez1_skip_incy4:
+   inc di
+
+   loop linez1_loop
+   
+
+linez1_no_iter:
+
+linez1_patch6:
+   mov cl, 123          ; do remaining iterations (0-3)
+   and cl, 03h
+
+   cmp cl, 0
+   je linez1_done                   
+        
+linez1_patch7:
+   mov es:[di], 123      ; draw pixel
+
+linez1_done:
+
+linez1_patch5:
+   mov sp, 1234
+   sti
+   
+   pop ds
+   pop si
+   pop di
+   pop bp
+   ret
+_cga_draw_line_zero1 ENDP
 
    END
 
