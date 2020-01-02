ec3bf9f711dd3791fa1d2a0a3104616897f8bcff
William Hart <goodwillhart@googlemail.com>
2020-01-02 05:51:30 +0100

    Some corrections and slower decay.

--- fire.asm
+++ fire.asm
@@ -19,6 +19,10 @@ _cga_fire PROC
 
    mov ax, ds
    mov es, ax
+
+   mov ax, 0b800h
+   mov ds, ax
+
    mov di, [rand_arr]
    xor bp, bp
 
@@ -33,7 +37,7 @@ cga_fire_loop:
    xor bp, bp
 cga_fire_more_rand:
 
-   mov si, 16192          ; copy random values just below visible screen
+   mov si, 8000          ; copy random values just below visible screen
    mov cx, 80
 cga_fire_rand_loop:
    mov al, es:[di+bp]
@@ -43,9 +47,6 @@ cga_fire_rand_loop:
    loop cga_fire_rand_loop
 
    push bp
-
-   mov ax, 0b800h
-   mov ds, ax
    
    mov di, 6000       ; setup di, bp
    mov bp, 8193
@@ -58,10 +59,10 @@ cga_fire_loop50:
    mov cx, 26         ; 78/3 = 26 iterations (unroll by three)
 
    sub bp, 2
-   mov dl, [di+bp]    ; setup al, ah, bl, bh, dl
+   mov dl, ds:[di+bp]    ; setup al, ah, bl, bh, dl
    inc bp
    and dl, 15
-   mov bl, [di+bp]
+   mov bl, ds:[di+bp]
    inc bp
    mov bh, bl
    shr bh, 1
@@ -69,7 +70,7 @@ cga_fire_loop50:
    shr bh, 1
    shr bh, 1
    and bl, 15
-   mov al, [di+bp]
+   mov al, ds:[di+bp]
    mov ah, al
    shr ah, 1
    shr ah, 1
@@ -81,7 +82,13 @@ cga_fire_loop26:
    add ah, bl         ; compute sums
    add al, ah
    add ah, bh
-   shr ax, 1          ; compute pixels
+   mov si, ax         ; compute pixels
+   shl si, 1
+   shl si, 1
+   add ax, si
+   shr ax, 1
+   shr ax, 1
+   shr ax, 1
    shr ax, 1
    shl al, 1
    shl al, 1
@@ -90,7 +97,7 @@ cga_fire_loop26:
    add al, ah
    mov [di], al       ; write pixels
    inc di
-   mov dh, [di+bp]    ; get next pixels
+   mov dh, ds:[di+bp]    ; get next pixels
    mov al, dh
    shr dh, 1
    shr dh, 1
@@ -101,7 +108,13 @@ cga_fire_loop26:
    add bh, dl         ; compute sums
    add bl, bh
    add bh, dh
-   shr bx, 1          ; compute pixels
+   mov si, bx         ; compute pixels
+   shl si, 1
+   shl si, 1
+   add bx, si
+   shr bx, 1
+   shr bx, 1
+   shr bx, 1
    shr bx, 1
    shl bl, 1
    shl bl, 1
@@ -110,7 +123,7 @@ cga_fire_loop26:
    add bl, bh
    mov [di], bl       ; write pixels
    inc di
-   mov ah, [di+bp]    ; get next pixels
+   mov ah, ds:[di+bp]    ; get next pixels
    mov bl, ah
    shr ah, 1
    shr ah, 1
@@ -121,7 +134,13 @@ cga_fire_loop26:
    add dh, al         ; compute sums
    add dl, dh
    add dh, ah
-   shr dx, 1          ; compute pixels
+   mov si, dx         ; compute pixels
+   shl si, 1
+   shl si, 1
+   add dx, si
+   shr dx, 1
+   shr dx, 1
+   shr dx, 1
    shr dx, 1
    shl dl, 1
    shl dl, 1
@@ -130,7 +149,7 @@ cga_fire_loop26:
    add dl, dh
    mov [di], dl       ; write pixels
    inc di
-   mov bh, [di+bp]    ; get next pixels
+   mov bh, ds:[di+bp]    ; get next pixels
    mov dl, bh
    shr bh, 1
    shr bh, 1
@@ -144,7 +163,13 @@ cga_fire_loop26:
    add ah, bl         ; compute sums
    add al, ah
    add ah, bh
-   shr ax, 1          ; compute pixels
+   mov si, ax         ; compute pixels
+   shl si, 1
+   shl si, 1
+   add ax, si
+   shr ax, 1
+   shr ax, 1
+   shr ax, 1
    shr ax, 1
    shl al, 1
    shl al, 1
@@ -153,7 +178,7 @@ cga_fire_loop26:
    add al, ah
    mov [di], al       ; write pixels
    inc di
-   mov dh, [di+bp]    ; get next pixels
+   mov dh, ds:[di+bp]    ; get next pixels
    mov al, dh
    shr dh, 1
    shr dh, 1
@@ -164,7 +189,13 @@ cga_fire_loop26:
    add bh, dl         ; compute sums
    add bl, bh
    add bh, dh
-   shr bx, 1          ; compute pixels
+   mov si, bx         ; compute pixels
+   shl si, 1
+   shl si, 1
+   add bx, si
+   shr bx, 1
+   shr bx, 1
+   shr bx, 1
    shr bx, 1
    shl bl, 1
    shl bl, 1
@@ -176,7 +207,7 @@ cga_fire_loop26:
 
    sub di, 8192
    jnc cga_fire_odd
-   add di, 16384
+   add di, 16304
 cga_fire_odd:
 
    xor bp, 0c050h     ; switch between 8193 and -8111
