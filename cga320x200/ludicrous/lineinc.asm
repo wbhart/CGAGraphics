2329cb62741b55417521a96e36be3c36b8e9316b
William Hart <goodwillhart@googlemail.com>
2020-07-26 15:11:32 +0200

    Attempt hr branch of get_line_increments.

--- cga320x200/ludicrous/lineinc.asm
+++ cga320x200/ludicrous/lineinc.asm
@@ -16,9 +16,18 @@ _get_line_increments PROC
    mov ax, ds           ; set segment for table
    mov es, ax
 
-   mov dx, [deltax]     ; compute increment = round(65536*dx/dy)
-   xor ax, ax
+   mov dx, [deltax]
    mov cx, [deltay]
+
+   cmp dx, 0
+   jl lineinc_left
+
+   cmp dx, cx
+   jb lineinc_vr
+   jmp lineinc_hr
+
+lineinc_vr:
+   xor ax, ax           ; compute increment = round(65536*dx/dy)
    div cx
    shl dx, 1
    adc ax, 0
@@ -60,6 +69,115 @@ lineinc_vr_no_iter:
    pop di
    pop bp
    ret
+
+lineinc_hr:
+   mov ax, dx           ; compute q = dx/dy
+   div cl
+   mov bh, al
+   mov dl, ah
+   xor dh, dh
+
+   xor ax, ax           ; compute increment = round(65536*dx/dy)
+   div cx
+   shl dx, 1
+   adc ax, 0
+   mov dx, ax
+
+   mov di, [buff]       ; get buffer address
+   inc di               ; skip first entry
+
+   mov ah, bh           ; get q
+
+   mov bx, 08000h       ; starting value for increments
+
+   add bx, dx           ; extra inc x?
+   salc
+   neg al
+   add al, ah
+
+   inc cx
+   shr cx, 1            ; compute iterations
+
+   jnc lineinc_hr_mid
+   jz lineinc_hr_no_iter
+
+lineinc_hr_loop:
+   stosb
+
+   add bx, dx           ; extra inc x?
+   salc
+   neg al
+   add al, ah
+
+lineinc_hr_mid:
+   stosb
+
+   add bx, dx           ; extra inc x?
+   salc
+   neg al
+   add al, ah
+
+   loop lineinc_hr_loop
+
+   pop di
+   pop bp
+   ret
+
+lineinc_left:
+
+   neg dx               ; make deltax positive
+
+   cmp dx, cx
+   jbe lineinc_vl
+   jmp lineinc_hl
+
+lineinc_vl:
+   xor ax, ax           ; compute increment = round(65536*dx/dy)
+   div cx
+   shl dx, 1
+   adc ax, 0
+   mov dx, ax
+
+   mov di, [buff]       ; get buffer address
+   inc di               ; skip first entry
+
+   mov bx, 08000h       ; starting value for increments
+
+   add bx, dx           ; inc x?
+   salc
+
+   inc cx
+   shr cx, 1            ; compute iterations
+
+   jnc lineinc_vl_mid
+   jz lineinc_vl_no_iter
+
+lineinc_vl_loop:
+   stosb
+
+   add bx, dx           ; inc x?
+   salc
+
+lineinc_vl_mid:
+   stosb
+
+   add bx, dx           ; inc x?
+   salc
+
+   loop lineinc_vl_loop
+
+lineinc_vl_no_iter:
+
+   pop di
+   pop bp
+   ret
+
+lineinc_hl:
+
+   pop di
+   pop bp
+   ret
+
 _get_line_increments ENDP
 
    END
