18c8e606ac1f0fb4777629113a711f7082e7ce47
William Hart <goodwillhart@googlemail.com>
2020-08-23 13:15:57 +0200

    Try to save a register.

--- cga320x200/ludicrous/polyfill.asm
+++ cga320x200/ludicrous/polyfill.asm
@@ -36,44 +36,42 @@ _cga_poly_fill PROC
 
    les di, buff         ; get buffer address in es:di
 
-   mov ch, [colour]     ; put solid colour in ch
+   mov dh, [colour]     ; put solid colour in ch
 
    mov bx, [y]          ; set dx to offset of CGA bank (odd/even)
 	shr bx, 1
    jnc poly_fill_even_y
    add di, 8192
-   ror ch, 1            ; adjust colour pattern for odd line
-   ror ch, 1
+   ror dh, 1            ; adjust colour pattern for odd line
+   ror dh, 1
 poly_fill_even_y:
 
    shl bx, 1
    add di, [bx+line_offset]
 
-   mov si, [inc1]       ; get addresses of increments buffers
-   mov ax, [inc2]
-   sub ax, si
-   mov WORD PTR cs:[inc_patch1+2], ax
-   mov WORD PTR cs:[inc_patch2+2], ax
-
    mov ax, [x1]
-   mov dx, [x2]
-   dec dx               ; rightmost pixel is not drawn
+   mov cx, [x2]
+   dec cx               ; rightmost pixel is not drawn
 
-   mov bp, [len]        ; get number of horizontal lines
+   mov dl, BYTE PTR [len] ; get number of horizontal lines
                         ; last line is not drawn
 
+   mov si, [inc1]       ; get addresses of increments buffers
+   mov bp, [inc2]
+
    inc si               ; skip first increments
-   
+   inc bp
+
    push ax
-   push dx
+   push cx
    push di
 
 poly_fill_short_loop:
-   cmp ax, dx           ; lines of non-positive length are skipped
+   cmp ax, cx           ; lines of non-positive length are skipped
    ja poly_fill_skip_line
 
    mov bh, al           ; set bh to 2*(x0 mod 4)
-   mov bl, dl           ; set bl to 2*(x1 mod 4)
+   mov bl, cl           ; set bl to 2*(x1 mod 4)
    and bx, 0303h
    shl bx, 1
 
@@ -81,23 +79,16 @@ poly_fill_short_loop:
    shr ax, 1
    add di, ax
 
-   shr dx, 1            ; set dx to x1/4 - x0/4 (final offset - initial offset)
-   shr dx, 1
-   sub dx, ax
+   shr cx, 1            ; set dx to x1/4 - x0/4 (final offset - initial offset)
+   shr cx, 1
+   sub cx, ax
 
    mov bx, [bx+masks]     ; left mask in bh, right mask in bl
 
-;   mov bh, 0ffh         ; prepare left mask in bh
-;   shr bh, cl
+   mov al, dh           ; copy colour in al and ah
+   mov ah, dh
 
-;   mov cl, bl           ; prepare right mask in bl
-;   mov bl, 0c0h
-;   sar bl, cl
-
-   mov al, ch           ; copy colour in al and ah
-   mov ah, ch
-
-   dec dl               ; if first and last pixels are not in the same byte
+   dec cl               ; if first and last pixels are not in the same byte
    jns poly_fill_long_line
 poly_fill_short_line:
 
@@ -111,8 +102,8 @@ poly_fill_short_line:
    
 poly_fill_skip_line:
 
-   ror ch, 1
-   ror ch, 1
+   ror dh, 1
+   ror dh, 1
 
    pop di
    sub di, 8112         ; increment y
@@ -124,25 +115,26 @@ poly_fill_skip_line:
    pop ax
 
    mov bx, ax
-inc_patch1:
-   mov al, BYTE PTR [si+200]
+
+   mov al, BYTE PTR [bp]
    cbw
-   add dx, ax
+   add cx, ax
    mov al, BYTE PTR [si]
    cbw
    add ax, bx
 
    inc si
+   inc bp
 
    push ax
-   push dx
+   push cx
    push di
 
-   dec bp
+   dec dl
    jnz poly_fill_short_loop
 
    pop di
-   pop dx
+   pop cx
    pop ax
 
    pop si
@@ -151,11 +143,11 @@ inc_patch1:
    ret
 
 poly_fill_long_loop:
-   cmp ax, dx           ; lines of non-positive length are skipped
+   cmp ax, cx           ; lines of non-positive length are skipped
    ja poly_fill_skip_line
 
    mov bh, al           ; set bh to 2*(x0 mod 4)
-   mov bl, dl           ; set bl to 2*(x1 mod 4)
+   mov bl, cl           ; set bl to 2*(x1 mod 4)
    and bx, 0303h
    shl bx, 1
 
@@ -163,16 +155,16 @@ poly_fill_long_loop:
    shr ax, 1
    add di, ax
 
-   shr dx, 1            ; set dx to x1/4 - x0/4 - 1 (final offset - initial offset - 1)
-   shr dx, 1
-   sub dx, ax
+   shr cx, 1            ; set dx to x1/4 - x0/4 - 1 (final offset - initial offset - 1)
+   shr cx, 1
+   sub cx, ax
 
    mov bx, [bx+masks]     ; left mask in bh, right mask in bl
 
-   mov al, ch           ; put colour into al and ah
-   mov ah, ch
+   mov al, dh           ; put colour into al and ah
+   mov ah, dh
 
-   dec dx
+   dec cx
    js poly_fill_short_line
 
 poly_fill_long_line:
@@ -184,13 +176,14 @@ poly_fill_long_line:
    stosb
 
    mov al, ah           ; draw full colour bytes
-   mov cx, dx
    shr cx, 1
    jnc poly_fill_even_iter
    stosb
 poly_fill_even_iter:
    rep stosw
-              
+
+   rcl cx, 1
+
    and al, bl           ; put right hand mask in bl and colour in al
    not bl
    
@@ -198,10 +191,10 @@ poly_fill_even_iter:
    or al, bl
    stosb
 
-   mov ch, ah           ; save colour
+   mov dh, ah           ; save colour
 
-   ror ch, 1
-   ror ch, 1
+   ror dh, 1
+   ror dh, 1
 
    pop di
    sub di, 8112         ; increment y
@@ -209,28 +202,30 @@ poly_fill_even_iter:
    and ax, 16304
    add di, ax
 
-   pop dx
+   pop cx
    pop ax
 
    mov bx, ax           ; get next increments
-inc_patch2:
-   mov al, BYTE PTR [si+200]
+
+   mov al, BYTE PTR [bp]
    cbw
-   add dx, ax
+   add cx, ax
    mov al, BYTE PTR [si]
    cbw
    add ax, bx
 
    inc si
+   inc bp
+
    push ax
-   push dx
+   push cx
    push di
 
-   dec bp
+   dec dl
    jnz poly_fill_long_loop
    
    pop di
-   pop dx
+   pop cx
    pop ax
 
    pop si
